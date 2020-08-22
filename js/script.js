5e4d7f5ad2cdf2d90cc05722d3a796f6dc9b042c
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-08-22 19:24:20 +0200

    JS

--- js/script.js
+++ js/script.js
@@ -1,41 +1,59 @@
-var genera = document.getElementById("genera");
+// GENERA TICKET
+var btmGenera = document.getElementById("genera");
 
-genera.addEventListner("click"
+btmGenera.addEventListner("click",
   function(){
-    var km =document.getElementById("nome").value;
-    var km =document.getElementById("km").value;
-    var km =document.getElementById("fascia-eta").value;
+    // RECUPOERO DATI INSERITI DALL'UTENTE
+    var nome = document.getElementById("nome").value;
+    var km = document.getElementById("km").value;
+    var eta = document.getElementById("fascia-eta").value;
     // value prende il valore dall'input
+
+    // CALCOLO DEL PREZZO DEL BIT
+    var prezzoKm = 0.21;
+
+    // PREZZO TOTALE BIT STANDARD
+    var totale = km * prezzoKm;
+    var topoOfferta = "Biglietto Standard";
+
+    // SCONTO PER MINORENNI 20%
+    if (fasciaEta =='minorenne'){
+      totale = totale - ((totale * 20) / 100);
+      tipoOfferta = "Sconto Minorenne";
+    }
+    // SCONTO PER OVER 65
+    else if (fasciaEta == 'over65'){
+      totale = totale - ((totale * 40) / 100);
+      tipoOfferta = "Sconto Silver";
+    }
+
+    // ARROTONDAMENTO A DUE CIFRE DECIMALI
+    totale = totale.toFixed(2);
+
+    // GENERO I NUMERI RANDOM PER LA CARROZZA E I CODICI CP
+    var numCarrozza = Math.floor(Math.random() * 9)+ 1;
+    var codiceCp = Math.floor(Math.random() * 9999);
+    
+    // STAMPA I DATI NEL BIGLIETTO
+    document.getElementById("nome-passeggero").innerHTML = nome;
+    document.getElementById("offerta").innerHTML = tipoOfferta;
+    document.getElementById("corrozza").innerHTML = carrozza;
+    document.getElementById("codice-cp").innerHTML = "9" + codice-cp;
+    document.getElementById("costo").innerHTML = totale + "Euro";
+    document.getElementById("biglietto-container").classList.add = ("show");
+
+
+
+
   }
 );
 
-// SCONTO PER MINORENNI
-if (eta<18){
-  var prezzoMin = (prezzo - ((prezzo / 100)* 20));
-  document.getElementById("prezzo-bit").innerHTML = prezzoMin.toFixed(2) + ", ed e' stato applicato uno sconto del 20%.";
-}
-// SCONTO PRE ANZIANI
-else if (eta>65){
-  var prezzoOver = (prezzo - ((prezzo / 100)* 40));
-  document.getElementById("prezzo-bit").innerHTML = prezzoOver.toFixed(2) + ", ed e' stato applicato uno sconto del 40%.";
-}
-// NESSUNO SCONTO
-else {
-  document.getElementById("prezzo-bit").innerHTML = prezzo.toFixed(2);
-}
 
-totale = totale.toFixed(2);
-var numCarrozza = Math.floor(Math.random() * 9)+ 1;
-var codiceCp = Math.floor(Math.random() * 9999);
 
-// STAMPA I DATI NEL BIGLIETTO
-document.getElementById("nome-passaggero").innerHTML = nome;
-document.getElementById("offerta").innerHTML = offerta;
-document.getElementById("corrozza").innerHTML = carrozza;
-document.getElementById("codice-cp").innerHTML = "9" + codice-cp;
-document.getElementById("costo").innerHTML = totale + "Euro";
 
-document.getElementById("biglietto-container").classList.add = ("show");
+
+
+
 
 
 
